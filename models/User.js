8f1e4411f24d83d756152438989b2e07bdb87b85
Aman-Islam-2003 <amanislam019@gmail.com>
2023-10-12 23:21:39 +0500

    Update User.js

--- models/User.js
+++ models/User.js
@@ -4,26 +4,51 @@ import validator from "validator";
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: [true, "Please enter your name"]
+    required: [true, "Please enter your name"],
   },
   email: {
     type: String,
     required: [true, "Please enter your email"],
     unique: true,
-    validate: validator.isEmail
+    validate: validator.isEmail,
   },
   password: {
     type: String,
     required: [true, "Please enter your password"],
     minLength: [6, "Password must be atleast 6 characters"],
-    select: false
+    select: false,
   },
   role: {
-    type: E,
-    required: [true, "Please enter your password"],
-    minLength: [6, "Password must be atleast 6 characters"],
-    select: false 
-  }
-})
+    type: toString,
+    enum: ["admin", "user"],
+    default: "user",
+  },
+  avatar: {
+    public_id: {
+      type: String,
+      required: true,
+    },
+    url: {
+      type: String,
+      required: true,
+    },
+  },
+
+  playlist: [
+    {
+      course: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Course",
+      },
+      poster: String,
+    },
+  ],
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  ResetPasswordToken: String,
+  ResetPasswordExpie: String,
+});
 
 export const User = mongoose.model("User", userSchema);
