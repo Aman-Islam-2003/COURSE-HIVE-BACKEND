38abd401253fd6d983bdbe8589114abc4c3d74c8
Aman-Islam-2003 <amanislam019@gmail.com>
2023-10-12 23:55:07 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
 const app = express();
+import courseRouter from "./routes/CourseRoutes.js"
+import userRouter from "./routes/userRoutes.js";
 
 dotenv.config({
     path: "./config/config.env"
 })
 
-
-
+app.use("/api/v1", courseRouter);
+app.use("/api/v2", userRouter);
 export default app;
