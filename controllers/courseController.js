6bcaacbfae3cc9148065b81691e230ea824aa052
Aman-Islam-2003 <amanislam019@gmail.com>
2023-10-13 23:58:04 +0500

    Update courseController.js

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -1,5 +1,9 @@
 import { Course } from "../models/Course.js";
 
-export const getAllCourses = (req,res,next) => {
-
+export const getAllCourses = async (req,res,next) => {
+   const courses = await Course.find();
+   res.status(200).send({
+    success: true,
+    courses
+   })
 }
