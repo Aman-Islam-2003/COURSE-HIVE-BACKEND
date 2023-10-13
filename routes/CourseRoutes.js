b97ed0d4f127167091753e29c64d3d0e4c6443eb
Aman-Islam-2003 <amanislam019@gmail.com>
2023-10-13 23:58:11 +0500

    Update CourseRoutes.js

--- routes/CourseRoutes.js
+++ routes/CourseRoutes.js
@@ -1,8 +1,8 @@
 import { Router } from "express";
-//import { getAllCourses } from "../controllers/courseController.js";
+import { getAllCourses } from "../controllers/courseController.js";
 const router = Router();
 
-   //router.route("/").get(getAllCourses) 
+   router.route("/").get(getAllCourses) 
 
 
 export default router;
