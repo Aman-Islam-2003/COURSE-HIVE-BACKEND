cdbe2a758c0f84e40620cc33dfd6f97d3016c319
Aman-Islam-2003 <amanislam019@gmail.com>
2023-10-14 19:19:46 +0500

    Update courseController.js

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -1,9 +1,16 @@
+import { catchAsyncError } from "../middlewares/catchAsyncError.js";
 import { Course } from "../models/Course.js";
 
-export const getAllCourses = async (req,res,next) => {
-   const courses = await Course.find();
+
+export const createCourse = catchAsyncError((req,res,next)=>{
+   const {title, description, category, createdBy } = req.body;
+   const file = req.file;
+})
+
+export const getAllCourses = catchAsyncError((req,res,next) => {
+   const courses =  Course.find();
    res.status(200).send({
     success: true,
     courses
    })
-}
+})
