66b1bbc28f945417a0431ca26ef4d96ccae8ecbc
Aman-Islam-2003 <amanislam019@gmail.com>
2023-10-14 19:19:36 +0500

    Update app.js

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ import dotenv from "dotenv";
 const app = express();
 import courseRouter from "./routes/CourseRoutes.js"
 import userRouter from "./routes/userRoutes.js";
+import {errorMiddleware} from "./middlewares/error.js"
 
 dotenv.config({
     path: "./config/config.env"
 })
 
 app.use("/api/v1", courseRouter);
-app.use("/api/v2", userRouter);
+app.use("/api/v1", userRouter);
+app.use(errorMiddleware);
 export default app;
