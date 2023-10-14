69baaac1faef08da35772d0b4ae4ae804f66616c
Aman-Islam-2003 <amanislam019@gmail.com>
2023-10-14 19:19:43 +0500

    Update catchAsyncError.js

--- middlewares/catchAsyncError.js
+++ middlewares/catchAsyncError.js
@@ -1,5 +1,3 @@
-export const catchAsyncError = ()=>{
-    return ()=>{
-        
-    }
+export const catchAsyncError = (passedFunction) => (req,res,next) => {
+    promise.resolve(passedFunction(req,res,next)).catch(next);//catch block needs an arrow ftn but we just pass the function don't call it because it will do it itself
 }
