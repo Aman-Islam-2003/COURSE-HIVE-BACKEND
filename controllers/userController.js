08f9c44e15796203fdfdb19e2e9332e2630eb5a2
Aman-Islam-2003 <amanislam019@gmail.com>
2023-10-15 23:40:06 +0500

    Update userController.js

---
+++ controllers/userController.js
@@ -0,0 +1,30 @@
+import { catchAsyncError } from "../middlewares/catchAsyncError.js";
+import {User} from "../models/User.js";
+import ErrorHandler from "../utils/errorHandler.js";
+import { sendToken } from "../utils/sendToken.js";
+
+export const register = catchAsyncError(async (req,res,next)=>{
+    const {name, email, password} = req.body;
+    //const file = req.file
+    if(!name || !email || ! password){
+        return next(new ErrorHandler("Required fields can't be empty", 400))
+    }
+    let user = await User.findOne({email});
+    if(user){
+        return next(new ErrorHandler("User already exists", 409))
+    }
+    //upload file n cloudinary
+
+    user = await User.create({
+       name,
+       email,
+       password,
+       avatar:{
+        public_id:"tempid",
+        url: tempurl
+       } 
+    });
+
+    sendToken(res, user, "Registered successfully", 201 );
+
+})
