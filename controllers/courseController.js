4051c54658db419d119fc845bcbb42ecc893e3d6
Aman-Islam-2003 <amanislam019@gmail.com>
2023-10-15 23:40:00 +0500

    Update courseController.js

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -1,14 +1,33 @@
 import { catchAsyncError } from "../middlewares/catchAsyncError.js";
 import { Course } from "../models/Course.js";
+import ErrorHandler from "../utils/errorHandler.js"
 
 
-export const createCourse = catchAsyncError((req,res,next)=>{
+export const createCourse = catchAsyncError(async (req,res,next)=>{
    const {title, description, category, createdBy } = req.body;
+   if(!title || !description || !category || !createdBy){
+       return next(new ErrorHandler("Required fields can't be empty", 400))
+   }
    const file = req.file;
+    await Course.create({
+      title,
+      description,
+      category,
+      createdBy,
+      poster: {
+         public_id: "temp",
+         url: "temp"
+      }
+   });
+
+   res.status(201).json({
+      success: "true",
+      message: "Course created successfully"
+   })
 })
 
-export const getAllCourses = catchAsyncError((req,res,next) => {
-   const courses =  Course.find();
+export const getAllCourses = catchAsyncError(async (req,res,next) => {
+   const courses = await Course.find().select("-lectures");
    res.status(200).send({
     success: true,
     courses
