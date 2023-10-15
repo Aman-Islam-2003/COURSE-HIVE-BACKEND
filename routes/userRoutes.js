80c01d0e9853d7dc58a2296bad0a2b92df00e6ab
Aman-Islam-2003 <amanislam019@gmail.com>
2023-10-15 23:40:13 +0500

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,8 @@
 import { Router } from "express";
-
+import {register} from "../controllers/userController.js"
 const router = Router();
 
-
+router.route("/register").post(register);
 
 
 export default router;
