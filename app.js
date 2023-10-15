05d3409175fa67e8eeb027e39dd696dddc7f764f
Aman-Islam-2003 <amanislam019@gmail.com>
2023-10-15 23:39:54 +0500

    Update app.js

--- app.js
+++ app.js
@@ -9,6 +9,11 @@ dotenv.config({
     path: "./config/config.env"
 })
 
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+}))
+
 app.use("/api/v1", courseRouter);
 app.use("/api/v1", userRouter);
 app.use(errorMiddleware);
