321ec4116034df9b3973b299174e737858d0eda6
Aman-Islam-2003 <amanislam019@gmail.com>
2023-10-18 23:56:25 +0500

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,11 +1,11 @@
 import { Router } from "express";
-import {register, login} from "../controllers/userController.js"
+import {register, login, logout, getMyProfile} from "../controllers/userController.js"
 const router = Router();
 
 router.route("/register").post(register);
 
 router.route("/login").post(login);
 router.route("/logout").get(logout);
-//router.route("/getMyProfile").post(register);
+router.route("/getMyProfile").post(getMyProfile);
 
 export default router;
