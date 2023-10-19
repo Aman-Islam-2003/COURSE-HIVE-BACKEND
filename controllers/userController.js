4a5394a63ef46f23fc47937dca566d7322a2316e
Aman-Islam-2003 <amanislam019@gmail.com>
2023-10-19 23:28:21 +0500

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -15,11 +15,10 @@ export const register = catchAsyncError(async (req,res,next)=>{
         return next(new ErrorHandler("User already exists", 409))
     }
     //upload file n cloudinary
-     const hashedPassword = await bcrypt.hash(password, 10);
     user = await User.create({
        name,
        email,
-       password: hashedPassword,
+       password,
        avatar:{
         public_id:"tempid",
         url: "tempurl",
@@ -62,10 +61,56 @@ export const logout = catchAsyncError(async (req,res,next)=>{
 export const getMyProfile = catchAsyncError(async (req,res,next)=>{
     const user = await User.findById(req.user._id);
     
-
     res.status(200).json({
         success: true,
         message: "Logged out successfully", 
         user
+    })
+})
+
+export const changePassword = catchAsyncError(async (req,res,next)=>{
+    const {oldPassword, newPassword} = req.body;
+
+    if(!oldPassword || !newPassword){
+        return next(new ErrorHandler("Required fields can't be empty", 400))
+    }
+    const user = await User.findById(req.user._id).select("+password");
+
+    const matchPasswords = bcrypt.compare(user.password, oldPassword);
+    if(!matchPasswords){
+        return next(new ErrorHandler("Incorrect password", 401))
+    }
+
+    user.password = newPassword;
+    await user.save();
+     
+    
+    res.status(200).json({
+        success: true,
+        message: "Password changed successfully", 
+        user
+    })
+})
+
+export const updateProfile = catchAsyncError(async (req,res,next)=>{
+    const {name, email} = req.body;
+    const user = await User.findById(req.user._id);
+
+    name = name? name: user.name;
+    email = email? email: user.email;
+    await user.save();
+       
+    res.status(200).json({
+        success: true,
+        message: "Profile updated successfully", 
+        user
+    })
+})
+
+export const updateProfilePicture = catchAsyncError(async (req,res,next)=>{
+   //cloudinary
+    res.status(200).json({
+        success: true,
+        message: "Profile Picture updated successfully", 
     })
 })
