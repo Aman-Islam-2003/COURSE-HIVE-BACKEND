595e859bffee49b48462df24d41b058ab0570c17
Aman-Islam-2003 <amanislam019@gmail.com>
2023-10-21 23:49:07 +0500

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import {register, login, logout, getMyProfile, changePassword, updateProfile, updateProfilePicture, forgetPassword, resetPassword} from "../controllers/userController.js"
+import {register, login, logout, getMyProfile, changePassword, updateProfile, updateProfilePicture, forgetPassword, resetPassword, addToPlaylist, removeFromPlaylist} from "../controllers/userController.js"
 import { isAuthenticated } from "../middlewares/Authenticate.js";
 const router = Router();
 
@@ -14,4 +14,7 @@ router.route("/updateProfilePicture").put(isAuthenticated, updateProfilePicture)
 router.route("/forgetpassword").put(isAuthenticated, forgetPassword);
 router.route("/resetPassword/:token").put(resetPassword);
 
+router.route("/addToPlaylist").post(isAuthenticated, addToPlaylist);
+
+router.route("/removeFromPlaylist").post(isAuthenticated, removeFromPlaylist);
 export default router;
