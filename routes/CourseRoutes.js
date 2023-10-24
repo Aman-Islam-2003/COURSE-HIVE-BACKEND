550bd8ff9c087df9fde90cee12748681ccf171fe
Aman Islam <amanislam019@gmail.com>
2023-10-24 23:44:18 +0500

    Update CourseRoutes.js

--- routes/CourseRoutes.js
+++ routes/CourseRoutes.js
@@ -9,7 +9,9 @@ router.route("/").get(getAllCourses); //without lectures
 router.route("/").post(createCourse); // only admin can create
 
 //Add lecture
+//router.route("/:id").post(addLecture); 
 //Delete Lecture
+//router.route("/:id").post(deleteLecture); // only admin can create
 //get course details
 //delete lecture
 
