ba8a7c75d81bdb7dce1c9aa420746bfc73921ec4
Aman-Islam-2003 <amanislam019@gmail.com>
2023-10-26 23:45:03 +0500

    Update server.js

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ import cloudinary from "cloudinary";
 
 connectDB();
 
-// cloudinary.v2.config({
-//    //cloud_name:dssau0rhs,
-//    // api_key: 835644199477898,
-//    // api_secret:WgB7tzMBgZh0KAi3bhBM-DaDiU8 
-// })
+cloudinary.v2.config({
+   cloud_name:process.env.CLOUDINARY_CLIENT_NAME,
+   api_key: process.env.CLOUDINARY_CLIENT_API,
+   api_secret:process.env.CLOUDINARY_CLIENT_SECRET 
+})
 
 const PORT = process.env.PORT;
 app.listen(PORT, ()=>{
