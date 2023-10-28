a11e7178dddc588437f17bf4256d5cc28dd633ce
Aman-Islam-2003 <amanislam019@gmail.com>
2023-10-28 23:48:30 +0500

    Update Authenticate.js

--- middlewares/Authenticate.js
+++ middlewares/Authenticate.js
@@ -20,8 +20,7 @@ export const isAuthenticated = catchAsyncError(async (req,res,next)=>{
 
 export const authorizeAdmin =  (req,res,next)=>{
     if(req.user.role != "admin"){
-        return next(new ErrorHandler(`$(req.user.role) is not allowed to access this course`, 403))
+        return next(new ErrorHandler(`${req.user.role} is not allowed to access this course`, 403))
     }
-
     next();
 };
