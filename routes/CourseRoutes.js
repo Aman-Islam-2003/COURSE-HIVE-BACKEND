65d0de3563f07ca432d0176b9d9424cca964f4e0
Aman-Islam-2003 <amanislam019@gmail.com>
2023-10-28 23:48:37 +0500

    Update CourseRoutes.js

--- routes/CourseRoutes.js
+++ routes/CourseRoutes.js
@@ -4,6 +4,8 @@ import {
   createCourse,
   getCourseLectures,
   addLecture,
+  deleteCourse,
+  deleteLecture
 } from "../controllers/courseController.js";
 import singleUpload from "../middlewares/multer.js";
 import { authorizeAdmin, isAuthenticated } from "../middlewares/Authenticate.js";
@@ -12,10 +14,10 @@ const router = Router();
 router.route("/courses").get(getAllCourses); //without lectures
 router.route("/createcourse").post(isAuthenticated, authorizeAdmin, singleUpload, createCourse); // only admin can create
 
-//Add lecture and get all lectures
-router.route("/course/:id").get(getCourseLectures).post(singleUpload, addLecture);
-//Delete Lecture
-//router.route("/course/:id").delete(deleteLecture)
+//Add lecture and get all lectures and delete course
+router.route("/course/:id").get(isAuthenticated, getCourseLectures).post(isAuthenticated, authorizeAdmin, singleUpload, addLecture).delete(isAuthenticated, authorizeAdmin, deleteCourse);
+//delete lecture
+router.route("/lecture").get(isAuthenticated, authorizeAdmin, deleteLecture)
 //get course details
 //delete lecture
 
