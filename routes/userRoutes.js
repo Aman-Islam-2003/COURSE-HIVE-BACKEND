8bae1d18fe88692c5a126321c933647cf297c228
Aman Islam <amanislam019@gmail.com>
2023-10-30 01:56:14 +0500

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,20 +1,28 @@
 import { Router } from "express";
-import {register, login, logout, getMyProfile, changePassword, updateProfile, updateProfilePicture, forgetPassword, resetPassword, addToPlaylist, removeFromPlaylist} from "../controllers/userController.js"
-import { isAuthenticated } from "../middlewares/Authenticate.js";
+import {register, login, logout, getMyProfile, changePassword, updateProfile, updateProfilePicture, forgetPassword, resetPassword, addToPlaylist, removeFromPlaylist, getAllUsers, updateUserRole} from "../controllers/userController.js"
+import { authorizeAdmin, isAuthenticated } from "../middlewares/Authenticate.js";
+import singleUpload from "../middlewares/multer.js";
+
 const router = Router();
 
-router.route("/register").post(register);
+router.route("/register").post(singleUpload, register);
 
 router.route("/login").post(login);
 router.route("/logout").get(logout);
 router.route("/me").get(isAuthenticated, getMyProfile);
 router.route("/changePassword").put(isAuthenticated, changePassword);
 router.route("/updateProfile").put(isAuthenticated, updateProfile);
-router.route("/updateProfilePicture").put(isAuthenticated, updateProfilePicture);
+router.route("/updateProfilePicture").put(singleUpload, isAuthenticated, updateProfilePicture);
 router.route("/forgetpassword").put(isAuthenticated, forgetPassword);
 router.route("/resetPassword/:token").put(resetPassword);
 
 router.route("/addToPlaylist").post(isAuthenticated, addToPlaylist);
 
 router.route("/removeFromPlaylist").post(isAuthenticated, removeFromPlaylist);
+
+//admin routes
+router.route("/admin.users").get(isAuthenticated, authorizeAdmin, getAllUsers);
+router.route("/admin.user/:id").put(isAuthenticated, authorizeAdmin, updateUserRole).delete(isAuthenticated, authorizeAdmin, deleteUser);
+
+router.route("/admin.user/:id").put(isAuthenticated, authorizeAdmin, updateUserRole).delete(isAuthenticated, authorizeAdmin, deleteUser);
 export default router;
