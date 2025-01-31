d9bb86657c049462f14d845154c11350f9d390f8
Aman Islam <amanislam019@gmail.com>
2025-01-31 23:36:59 +0500

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ import dotenv from "dotenv";
 const app = express();
 import courseRouter from "./routes/CourseRoutes.js"
 import userRouter from "./routes/userRoutes.js";
-import {errorMiddleware} from "./middlewares/error.js"
+import {errorMiddleware} from "./middlewares/error.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config({
     path: "./config/config.env"
@@ -12,7 +13,8 @@ dotenv.config({
 app.use(express.json());
 app.use(express.urlencoded({
     extended: true
-}))
+}));
+app.use(cookieParser());
 
 app.use("/api/v1", courseRouter);
 app.use("/api/v1", userRouter);
