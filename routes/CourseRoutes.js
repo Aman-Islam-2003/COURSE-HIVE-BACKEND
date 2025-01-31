230b4da65380b10f25042b7a638c66df5debadb0
Aman Islam <amanislam019@gmail.com>
2025-01-31 23:37:38 +0500

    Update CourseRoutes.js

--- routes/CourseRoutes.js
+++ routes/CourseRoutes.js
@@ -2,14 +2,21 @@ import { Router } from "express";
 import {
   getAllCourses,
   createCourse,
+  getCourseLectures,
+  addLecture,
 } from "../controllers/courseController.js";
+import singleUpload from "../middlewares/multer.js";
 const router = Router();
 
-router.route("/").get(getAllCourses); //without lectures
-router.route("/").post(createCourse); // only admin can create
+router.route("/courses").get(getAllCourses); //without lectures
+router.route("/createcourse").post(singleUpload, createCourse); // only admin can create
 
 //Add lecture
+<<<<<<< HEAD
 //router.route("/:id").post(addLecture); 
+=======
+router.route("/course/:id").get(getCourseLectures).post(singleUpload, addLecture);
+>>>>>>> 5d1876484704cf995cf8ff046d7321ff2edc5162
 //Delete Lecture
 //router.route("/:id").post(deleteLecture); // only admin can create
 //get course details
